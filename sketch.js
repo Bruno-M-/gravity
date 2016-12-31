4750678f3600919f13901533dfe704996937fa39
Bruno MAITRE <bruno.maitre@gmail.com>
2016-12-31 06:07:45 +0100

    Rework mouse shooting system and add circular orbit shooting system

--- sketch.js
+++ sketch.js
@@ -6,12 +6,28 @@ var count = 0;
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	sun = new Sun(windowWidth/2, windowHeight/2);
-	shoot = new shooter();
+	shoot = new Shooter();
 }
 
 function draw() {
 	background(255);
 
+	/* Check if a planet is out of bounds */
+	var bound = 5000;
+	var to_splice = [];
+	for (var i = 0; i < planets.length ; i++) {
+		if ((planets[i].pos.x > bound) ||
+		    (planets[i].pos.x < (0-bound)) ||
+		    (planets[i].pos.y > bound) ||
+		    (planets[i].pos.y < (0-bound))) {
+			append(to_splice, i);
+		}
+	}
+	for (var i = 0; i < to_splice.length ; i++) {
+		planets.splice(to_splice[i], 1);
+	}
+
+	/* Render planets */
 	for (var i = 0; i < planets.length ; i++) {
 		for (var j = 0; j < planets.length ; j++) {
 			if( i != j) {
@@ -23,25 +39,87 @@ function draw() {
 		planets[i].draw();
 	}
   
+	/* Draw the sun */
 	sun.draw();
 	
+	/* Render shooter */
 	shoot.update();
 	shoot.draw();
 }
 
-function shooter() {
-	var x1 = 0;
-	var y1 = 0;
-	var x2 = 0;
-	var y2 = 0;
-	var R = 0;
-	var G = 0;
-	var B = 0;
+function Shooter() {
+	this.x1 = 0;
+	this.y1 = 0;
+	this.x2 = 0;
+	this.y2 = 0;
+	this.R = 0;
+	this.G = 0;
+	this.B = 0;
 	this.force = 0;
-	var shooting=false;
+	this.force_x = 0;
+	this.force_y = 0;
+	this.shooting=false;
+	this.c_shooting=false;
+	this.touchKey=false;
 
 	this.update = function() {
-		if((!this.shooting) && (mouseIsPressed)) {
+
+		/* Circular shooter */
+		if((!this.shooting) && (keyIsPressed) && (key == "o") && (!this.c_shooting) && (!this.touchKey)) {
+			this.c_shooting = true;
+			this.touchKey = true;
+			this.R = floor(random(255));
+			this.G = floor(random(255));
+			this.B = floor(random(255));
+			this.force = 0;
+			this.force_x = 0;
+			this.force_y = 0;
+
+			if (abs(mouseX - windowWidth/2) < abs(mouseY - windowHeight/2)) {
+				this.x1 = windowWidth/2;
+				this.y1 = mouseY;
+				this.y2 = mouseY;
+				this.force = sqrt(10000/(abs((windowHeight/2) - mouseY)));
+				this.force_x = this.force;
+				this.x2 = this.x1 - (10 * this.force_x);
+
+			} else {
+				this.x1 = mouseX;
+				this.x2 = mouseX;
+				this.y1 = windowHeight/2;
+				this.force = sqrt(10000/(abs((windowWidth/2) - mouseX)));
+				this.force_y = this.force;
+				this.y2 = this.y1 - (10 * this.force_y);
+			}
+		}
+		if((!this.shooting) && (keyIsPressed) && (key == "o") && (this.c_shooting) && (!this.touchKey)) {
+			this.touchKey = true;
+			if(this.x1 == windowWidth/2) {
+				this.force_x = -this.force_x;
+				this.x2 = this.x1 - (10 * this.force_x);
+			} else if (this.y1 == windowHeight/2) {
+				this.force_y = -this.force_y;
+				this.y2 = this.y1 - (10 * this.force_y);
+			}
+		}
+
+		if((!this.shooting) && (keyIsPressed)  && (keyCode == ENTER) && (this.c_shooting) && (!this.touchKey)) {
+			this.c_shooting = false;
+			this.touchKey = true;
+			append(planets, new Planet(this.x1, this.y1,this.force_x,this.force_y,this.R,this.G,this.B,10));
+		}
+
+		if((!this.shooting) && (keyIsPressed)  && (keyCode == ESCAPE) && (this.c_shooting) && (!this.touchKey)) {
+			this.c_shooting = false;
+			this.touchKey = true;
+		}
+
+		if (!keyIsPressed) {
+			this.touchKey = false;
+		}
+
+		/* Mouse shooter */
+		if((!this.shooting) && (mouseIsPressed) && (!this.c_shooting)) {
 			this.shooting = true;
 			this.x1 = mouseX;
 			this.y1 = mouseY;
@@ -58,19 +136,17 @@ function shooter() {
 
 		if (!mouseIsPressed && this.shooting) {
 			var alpha = 0;
-			var force_x = 0;
-			var force_y = 0;
 			var x_dir = 0;
 			var y_dir = 0;
 
 			if (this.force > 1) {
 				if (this.x1 != this.x2) {
-					alpha = atan(abs((this.y1 - this.y2)) / abs((this.x1 - this.x2)));
-					force_x = this.force * cos(alpha);
-					force_y = this.force * sin(alpha);
+					alpha = atan(abs((this.y2 - this.y1)) / abs((this.x2 - this.x1)));
+					this.force_x = this.force * cos(alpha);
+					this.force_y = this.force * sin(alpha);
 				} else {
-					force_x = 0;
-					force_y = this.force;
+					this.force_x = 0;
+					this.force_y = this.force;
 				}	
 
 				if (this.x2 < this.x1) {
@@ -90,7 +166,7 @@ function shooter() {
 						y_dir = -1;
 					}
 				}
-				append(planets, new Planet(this.x1, this.y1, (x_dir * force_x),(y_dir * force_y),this.R,this.G,this.B,10));
+				append(planets, new Planet(this.x2, this.y2, (x_dir * this.force_x),(y_dir * this.force_y),this.R,this.G,this.B,10));
 			}
 			this.shooting = false;
 		}
@@ -98,17 +174,17 @@ function shooter() {
 
 	this.draw = function() {
 
-			if (this.shooting) {
+			if ((this.shooting) || (this.c_shooting)) {
 				/* Draw the line and the arraow */
 				stroke(0);
-				line(this.x2-5,this.y2, this.x2+5, this.y2);
-				line(this.x2,this.y2-5, this.x2, this.y2+5);
+				line(this.x1-5,this.y1, this.x1+5, this.y1);
+				line(this.x1,this.y1-5, this.x1, this.y1+5);
 		        	line(this.x1, this.y1, this.x2, this.y2);
 
 				/* Draw the futur planet */
 				noStroke();
 				fill(this.R,this.G,this.B);
-			    	ellipse(this.x1, this.y1, 20, 20);
+				ellipse(this.x2, this.y2, 20, 20);
 
 			        push();
 				fill(0);
@@ -153,6 +229,7 @@ function Planet(x,y,accx,accy,R,G,B,radius) {
 	this.draw = function() {
 		/* Draw planet */
 		push();
+		noStroke();
 		fill(this.R,this.G,this.B,250);
 		translate(this.pos.x, this.pos.y);
 		rotate(this.vel.heading());
